bbec0713aaef85fc6646ff1228cb53c85a1abf4d
Andrii Maliuta <quadr988@gmail.com>
2020-09-19 15:53:08 +0300

    init

--- src/main/java/com/anma/controllers/HomeController.java
+++ src/main/java/com/anma/controllers/HomeController.java
@@ -1,11 +1,13 @@
 package com.anma.controllers;
 
+import com.anma.models.Cat;
 import io.micronaut.core.util.CollectionUtils;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.views.View;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -15,6 +17,10 @@ public class HomeController {
     @Get("/")
     public HttpResponse<Map> getIndex() {
 
-        return HttpResponse.ok(CollectionUtils.mapOf("name", "Vasyl", "age", 34));
+        Map<String, Object> map = new HashMap<>();
+        map.put("cat", new Cat("Murko", 6));
+
+//        return HttpResponse.ok(CollectionUtils.mapOf("name", "Vasyl", "age", 34));
+        return HttpResponse.ok(map);
     }
 }
